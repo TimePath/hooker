89dc44abba1e84f3d073de78b84dece8ba9cd36c
TimePath <andrew.hardaker1995@gmail.com>
2014-09-09 11:17:12 +1000

    More tests

--- src/main/java/com/timepath/hooker/Test.java
+++ src/main/java/com/timepath/hooker/Test.java
@@ -4,8 +4,14 @@ public class Test {
 
     public static void main(String[] args) {
         test(args);
+        test(null);
+        test(new Object[]{null});
         test("a");
         test("a", "b");
+        test(new String[]{"c", "d"});
+        test(new String[]{"c", "d"}, "e");
+        new StaticInner().test();
+        StaticInner.test(7, 8);
         new Test().test();
     }
 
@@ -13,6 +19,20 @@ public class Test {
     }
 
     void test() {
+        new EnclosedInner().test();
+    }
+
+    static class StaticInner {
+        static void test(int o, int j) {
+        }
+
+        void test() {
+        }
+    }
+
+    class EnclosedInner {
+        void test() {
+        }
     }
 
 }
